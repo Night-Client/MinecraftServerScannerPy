77e7bc400d47bded256effc1cf053bc0013b9698
diuagsiudhasiud <spamsignup15@gmail.com>
2025-05-19 18:22:37 +0000

    kjhgdiuashfodisad

--- ServerScanner.py
+++ ServerScanner.py
@@ -7,17 +7,19 @@ MAX_THREADS = 600
 filepath = input("Path to text file to save data in: ")
 last_scanned = 0
 total_scanned = 0
+found = 0
 
 def test():
     global total_scanned
+    global found
     file = open(filepath, "a+")
     ip = str(92) + "." + str(randint(100, 120)) + "." + str(randint(1, 255)) + "." + str(randint(1, 255)) + ":25565"
     server = JavaServer.lookup(ip)
 
     try:
-        print((server.status().version.__str__() + " Exists"))
         res = ip + " - " + server.status().version.__str__() + " -  Exists"
         file.write(res + "\n")
+        found += 1
     except:
         pass
 
@@ -27,8 +29,9 @@ def test():
 def status():
     global last_scanned
     global total_scanned
+    global found
     time.sleep(10)
-    print("Scanned " + str(total_scanned) + " Servers (" + str((total_scanned - last_scanned)/10) + "/s)")
+    print("Scanned " + str(total_scanned) + " Servers (" + str((total_scanned - last_scanned)/10) + "/s) And Found " + str(found) + " Servers")
     last_scanned = total_scanned
     status()
 
